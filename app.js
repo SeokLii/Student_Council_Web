b1610ce2c147aba6fc7958e060a5dbb7b0e3c722
LeeWansSeok <k1nder@naver.com>
2020-02-23 17:21:09 +0900

    기본적인 웹페이지를 위한 서버 및 홈페이지 구축

---
+++ app.js
@@ -0,0 +1,59 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var sassMiddleware = require('node-sass-middleware');
+var favicon = require('serve-favicon');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var introduceRouter = require('./routes/introduce');
+var noticeRouter = require('./routes/notice');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//serve-favicon
+app.use(favicon(path.join(__dirname, 'public/images', 'favicon.ico'))); //
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(sassMiddleware({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true, // true = .sass and false = .scss
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// interlink router (user)
+app.use('/', indexRouter); //Main page
+app.use('/introduce', introduceRouter); //Student Council introduce page
+app.use('/notice', noticeRouter); // Notice page
+//app.use('/commitment', commitmentRouter); //Commitment page
+
+// interlink router (manager)
+app.use('/users', usersRouter); // manager page
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
