6f6be46af760bf80c0f8ba3b36b8c8a138e58e9a
LeeWansSeok <k1nder@naver.com>
2020-02-23 20:59:15 +0900

    페이지 내용 추가 등록 및 공지사항 페이지 기본 구축

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var introduceRouter = require('./routes/introduce');
 var noticeRouter = require('./routes/notice');
+
 var app = express();
 
 // view engine setup
--- routes/notice.js
+++ routes/notice.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router(); //라우터 분리
+var mysql_odbc = require('./database/database_conn')();
+var conn = mysql_odbc.init();
 
-/* GET home page. */
-router.get('/notice', function(req, res, next) {
-  res.render('notice', { title: '제8대 소프트웨어학과 이룸학생회' });
+/* Board List page */
+router.get('/', function(req, res, next) {
+  var sql = "select * from persons";
+  conn.query(sql, function (err, rows) {
+    if (err) {
+      console.error("err : " + err);
+    }
+    else{
+      console.log(rows);
+      res.render('notice', { title: '제8대 소프트웨어학과 이룸학생회', rows: rows });
+    }
+  });
 });
+/*Post page*/
 
+
+/* Update page */
+
+
+/* Delete page */
+
+
+/* page */
 module.exports = router;
