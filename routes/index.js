6cb5c48eab5bede8925ce721f24ddaf7dab97508
LeeWansSeok <k1nder@naver.com>
2020-06-08 23:24:36 +0900

    ignore specific files test

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,16 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/test', function(req, res, next) {
+  var sql = "select number, title, content, studentID, name, grade, source, date_format(date, '%Y-%m-%d')date, imgPath from board";
+  conn.query(sql, function (err, rows) {
+    if (err) console.error("err : " + err);
+    console.log(rows.length);
+    if (!req.user) res.render('test', { logIO_L : 'login', logIO_T : '로그인', rows: rows, length: rows.length-1, pagenum: rows.length-16, pass: true });
+    else res.render('test', { logIO_L : 'logout', logIO_T : '로그아웃', rows : rows, length : rows.length-1, pagenum: rows.length-16, pass: true });
+  });
+});
+
 
 
 module.exports = router;
